86675134cff0aa9afb0a89407049b1b42cf74663
Nikita <nikjk495@gmail.com>
2018-07-03 19:01:58 +0300

    Refactor CountDownLatchExample

--- java-school-concurrent-examples/src/main/java/com/sbt/examples/CountdownLatchExample.java
+++ java-school-concurrent-examples/src/main/java/com/sbt/examples/CountdownLatchExample.java
@@ -1,25 +1,45 @@
 package com.sbt.examples;
 
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
 import java.util.concurrent.CountDownLatch;
 
 public class CountdownLatchExample {
+
+
+    private static final Set<Integer> set = new HashSet<Integer>();
+
+    public static synchronized void add(Integer i) {
+        set.add(i);
+    }
+
+    public synchronized void remove(Integer i) {
+        set.remove(i);
+    }
+
+    public static void addTenThings() {
+        Random r = new Random();
+        for (int i = 0; i < 10; i++)
+            add(r.nextInt());
+        System.out.println("DEBUG: added ten elements to" + set);
+    }
+
     public static long timeTask(int nThreads, final Runnable tasks) throws InterruptedException {
         final CountDownLatch startGate = new CountDownLatch(1);
         final CountDownLatch endGate = new CountDownLatch(nThreads);
         for (int i = 0; i < nThreads; i++) {
-            Thread t = new Thread(new Runnable() {
-                @Override
-                public void run() {
+            Thread t = new Thread(() -> {
+                try {
+                    startGate.await();
+                    System.out.println("StartGate await");
                     try {
-                        startGate.await();
-                        System.out.println("StartGate await");
-                        try {
-                            tasks.run();
-                        } finally {
-                            endGate.countDown();
-                        }
-                    } catch (InterruptedException e) {
+                        tasks.run();
+                    } finally {
+                        endGate.countDown();
+                        System.out.println("Start Gate open");
                     }
+                } catch (InterruptedException e) {
                 }
             }, String.valueOf(i));
             t.start();
@@ -34,12 +54,19 @@ public class CountdownLatchExample {
     }
 
     public static void main(String[] args) throws InterruptedException {
-        System.out.println(timeTask(10, () -> {
+        System.out.println(timeTask(10, new Runnable() {
+            @Override
+            public void run() {
+                addTenThings();
+            }
+        }));
+
+        /*   System.out.println(timeTask(10, () -> {
             try {
                 Thread.sleep((long) (Math.random() * 1000));
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-        }));
+        }));*/
     }
 }
